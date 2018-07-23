e6f480abef6cabceea5e51fdd34c45155df2e3c3
Codecamp <Codecamp@mac-mini.local>
2018-07-23 00:48:49 -0700

    Initial Commit

---
+++ Test/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Test
+//
+//  Created by Codecamp on 23.07.18.
+//  Copyright © 2018 Codecamp. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Test/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  Test
+//
+//  Created by Codecamp on 23.07.18.
+//  Copyright © 2018 Codecamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
